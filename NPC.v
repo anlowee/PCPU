65798ec1dc2cae66a9694b75b88f0fea326ec61d
Charge Goei <40865608+anlowee@users.noreply.github.com>
2020-05-14 09:10:49 +0800

    Update NPC.v
    Set NPC at ID stage, remove Zero and Gez signal from alu, add RegRt signal used for branch ins.

--- NPC.v
+++ NPC.v
@@ -1,13 +1,12 @@
 `include "ctrl_encode_def.v"
 
-module NPC(PC, NPCOp, Reg, IMM, NPC, Zero, Gez, PCPLUS4);  // next pc module
+module NPC(PC, NPCOp, RegRs, RegRt, IMM, NPC, PCPLUS4);  // next pc module
     
-   input Zero;  // ALU output: is equal to zero 
-   input Gez;   // ALU output: is greater/equal to zero
    input  [31:0] PC;        // pc
    input  [3:0]  NPCOp;     // next pc operation
    input  [25:0] IMM;       // immediate
-   input  [31:0] Reg;       // data read from RF, used in jalr and jr
+   input  [31:0] RegRs;       // rs data read from RF, used in jalr and jr
+   input  [31:0] RegRt;     // rt data read from RF, used in beq
    output reg [31:0] NPC;   // next pc
    output [31:0] PCPLUS4;  // used to store PC + 4 into %ra
    
@@ -17,25 +16,25 @@ module NPC(PC, NPCOp, Reg, IMM, NPC, Zero, Gez, PCPLUS4);  // next pc module
       case (NPCOp)
           `NPC_PLUS4:  NPC = PCPLUS4;
           `NPC_BRANCH_BEQ:    begin
-            NPC = (Zero) ? PCPLUS4 + {{14{IMM[15]}}, IMM[15:0], 2'b00} : PCPLUS4;
+            NPC = (RegRs == RegRt) ? PCPLUS4 + {{14{IMM[15]}}, IMM[15:0], 2'b00} : PCPLUS4;
           end
           `NPC_BRANCH_BGEZ:   begin
-            NPC = (Gez) ? PCPLUS4 + {{14{IMM[15]}}, IMM[15:0], 2'b00} : PCPLUS4;
+            NPC = (~RegRs[31]) ? PCPLUS4 + {{14{IMM[15]}}, IMM[15:0], 2'b00} : PCPLUS4;
           end
           `NPC_BRANCH_BGTZ:   begin
-            NPC = (Gez & ~Zero) ? PCPLUS4 + {{14{IMM[15]}}, IMM[15:0], 2'b00} : PCPLUS4;
+            NPC = (~RegRs[31] & ~(RegRs == 32'b0)) ? PCPLUS4 + {{14{IMM[15]}}, IMM[15:0], 2'b00} : PCPLUS4;
           end
           `NPC_BRANCH_BLEZ:   begin
-            NPC = (Zero | ~Gez) ? PCPLUS4 + {{14{IMM[15]}}, IMM[15:0], 2'b00} : PCPLUS4;
+            NPC = ((RegRs == 32'b0) | RegRs[31]) ? PCPLUS4 + {{14{IMM[15]}}, IMM[15:0], 2'b00} : PCPLUS4;
           end
           `NPC_BRANCH_BLTZ:   begin
-            NPC = (~Gez) ? PCPLUS4 + {{14{IMM[15]}}, IMM[15:0], 2'b00} : PCPLUS4;
+            NPC = (RegRs[31]) ? PCPLUS4 + {{14{IMM[15]}}, IMM[15:0], 2'b00} : PCPLUS4;
           end
           `NPC_BRANCH_BNE:    begin
-            NPC = (~Zero) ? PCPLUS4 + {{14{IMM[15]}}, IMM[15:0], 2'b00} : PCPLUS4;
+            NPC = (~(RegRs == RegRt)) ? PCPLUS4 + {{14{IMM[15]}}, IMM[15:0], 2'b00} : PCPLUS4;
           end
           `NPC_JUMP:   NPC = {PCPLUS4[31:28], IMM[25:0], 2'b00};
-          `NPC_JUMPR:  NPC = Reg;
+          `NPC_JUMPR:  NPC = RegRs;
           `NPC_NOP:   NPC = PC;
           default:     NPC = PC;
       endcase
